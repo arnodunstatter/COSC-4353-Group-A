a6780d0ed79cdd1b8fe4036ebc10b4cea03d6915
Arno Dunstatter <arnodunstatter@gmail.com>
2021-11-20 15:39:48 -0600

    1. got our constructor to read from .txt - tested with an undirected, unweighted, non-multigraph, and with a directed, weighted, multigraph - both worked NOTE: we still don't have input validation measures in place, if the .txt contradicts itself, the program will break or behave in unexpected/unpredictable ways - consider adding input validation later 2. wrote our graphGenerator (it's a __init__) - this hasn't been tested because of some weird bug with the named arguments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from classes import Graph
 import numpy as np
 
-g = Graph("toyInput.txt")
+#def __init__(self, seed, numNodes, numConnections, name="", date="", description="", weightsRange=None, isMultiGraph=False, isDirected=False, isWeighted=False):
+g = Graph(seed=1, numNodes=5, numConnections=4, name="MarkyMark", description="testing graph generator")
 
-# print(type(np.array([1])))
-# print(type(g.adjacencyMatrix.loc["A","D"]))
+print(g.adjacencyLists)
+print("\n")
+print(g.adjacencyMatrix)
