b655e3f50735d704243023a48c4d19ae1bc43d4f
Arno Dunstatter <arnodunstatter@gmail.com>
2021-12-02 00:58:31 -0600

    implementing feature forking

--- scratch.py
+++
@@ -1,5 +0,0 @@
-from classes import *
-
-g = Graph(seed=2, numNodes=5, numConnections=7, name="", date="", description="", weightsRange=[1,5], isMultiGraph=True, isDirected=True, isWeighted=True)
-
-
