bbe10a6de1d3fe5c4ed6b4ac229cfdc97f7b8d4d
Santos Garcia <89603557+JAIM307@users.noreply.github.com>
2021-12-02 02:37:41 -0600

    Added auxiliary files part 2

---
+++ scratch.py
@@ -0,0 +1,5 @@
+from classes import *
+
+g = Graph(seed=2, numNodes=5, numConnections=7, name="", date="", description="", weightsRange=[1,5], isMultiGraph=True, isDirected=True, isWeighted=True)
+
+
